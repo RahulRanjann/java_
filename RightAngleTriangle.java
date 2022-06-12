430e7472c62ba03c3cf2fd1e832ea6ebcb8e736d
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-06-12 10:00:07 +0530

    right triangle add

---
+++ RightAngleTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RightAngleTriangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Column no.");
+        int column = sc.nextInt();
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
