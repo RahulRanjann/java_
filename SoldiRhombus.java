4819f8b105094b0253ddee8a1191d2a56a973fd4
Rahul Ranjan <88098666+rahul191311054@users.noreply.github.com>
2022-06-17 19:07:39 +0530

    solid Rhombus

---
+++ SoldiRhombus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SoldiRhombus {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no. of row");
+        int row = sc.nextInt();
+
+        for (int i = 0; i <= row; i++) {
+            // space
+            for (int j = 0; j <= (row - i); j++) {
+                System.out.print(" ");
+            }
+            // for Rhombus
+            for (int j = 1; j <= row; j++) {
+                System.out.print("* ");
+            }    
+            System.out.println();
+        }
+
+    }
+}
