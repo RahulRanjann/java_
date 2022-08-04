6a4fd1422383930073ee73d299c98e63fbbe42bd
Rahul Ranjan <88098666+RahulRanjann@users.noreply.github.com>
2022-08-04 07:36:18 +0530

    java Class

---
+++ RectArea.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Rectangle{
+
+    int length, width; // instance variable
+
+    void getData(int x, int y){
+        length = x;
+        width = y;
+    }
+
+    int rectArea(){
+
+        int area = length*width;
+        return area;
+    }
+}
+
+
+class RectArea{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int area1, area2;
+        Rectangle rect1 = new Rectangle();
+        Rectangle rect2 = new Rectangle();
+        rect1.length = sc.nextInt();
+        rect1.width = 20;
+        area1 = rect1.length * rect1.width;
+        rect2.length = 15;
+        rect2.width = 25;
+        area2 = rect2.length * rect2.width; 
+        System.out.println("AREA1 ="+ area1);
+        System.out.println("AREA2="+ area2);
+    }
+}
