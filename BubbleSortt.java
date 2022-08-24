9e2428bdee58b333a6f1c9a85a41dec7fb8a6eec
RahulRanjann <rahul1913111054@gmail.com>
2022-08-24 19:40:45 +0530

    Bubble sorting

---
+++ BubbleSortt.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BubbleSortt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of array");
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        System.out.println("Enter the");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println("array of "+i);
+            arr1[i]= sc.nextInt();
+        }
+
+        //bubble sort
+        for (int i = 0; i < arr1.length; i++) {
+            int temp ;
+         
+            for (int j = 0; j < (arr1.length-1); j++) {
+                
+                if (arr1[j] > arr1[j+1]) {
+                    temp = arr1[j];
+                    arr1[j] = arr1[j+1];
+                    arr1[j+1] = temp; 
+                }
+            }            
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println("After sorting the array "+arr1[i]);
+        }
+    }
+}
