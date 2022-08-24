a0f493ef6c8baa72628027b378a2a07140ad0b8d
RahulRanjann <rahul1913111054@gmail.com>
2022-08-24 22:54:53 +0530

    simple Array

---
+++ arr1.java
@@ -0,0 +1,14 @@
+import javax.swing.SpinnerDateModel;
+
+public class arr1 {
+    public static void main(String[] args) {
+        int[] arr1= new int[5];
+        arr1[0]=4;
+        arr1[1]=40;
+        arr1[2]=43;
+        arr1[3]=44;
+        arr1[4]=45;
+        System.out.println(arr1[3]); // 44
+       
+    }
+}
