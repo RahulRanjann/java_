f8f707c33dc71ded1659936ab783f5be351ab762
RahulRanjann <rahul1913111054@gmail.com>
2022-10-08 23:21:01 +0530

    palendromNumber

---
+++ PalandromNo.java
@@ -0,0 +1,27 @@
+public class PalandromNo {
+    public static void main(String[] args) {
+        System.out.println("Enter Number you want to reverse: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int copy = a;
+        int reverse = 0;
+
+        int reminder;
+        while (a > 0) {
+            reminder = a % 10;
+            reverse = reverse * 10 + reminder;
+            a = a /10;
+            
+        }
+        
+        System.out.println("The reverse number is:" + reverse);
+
+       if (copy == reverse) {
+            System.out.println("is a palendrom");
+       } 
+       else {
+        System.out.println("not a palendrom");
+       }
+       
+    }
+}
